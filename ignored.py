f3041722e608e96c43c06044f83cc74f5f882daa
I'm GrooT <51871154+b0tdev@users.noreply.github.com>
2019-08-01 07:16:19 +0530

    Add files via upload

---
+++ ignored.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix="m-")
+
+@client.event
+async def on_ready():
+	print('Done')
+
+@client.command(name="load", discription="Loads a cog", hidden=True)
+async def load(ctx, extension):
+	client.load_extension(f"cogs.{extension}")
+	await ctx.send('Successfully loaded')
+	
+@client.command()
+async def unload(ctx, extension):
+	client.unload_extension(f"cogs.{extension}")
+	await ctx.send('Successfully unloaded')
+	
+for filename in os.listdir('./cogs'):
+	if filename.endswith('.py'):
+		client.load_extension(f"cogs.{filename[:-3]}")
+		
+client.run(os.getenv("TOKEN"))
